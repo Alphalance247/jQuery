2e68b5eff6c56955128915c8712d82556423ce15
Alphalance247 <al.moshood017@gmail.com>
2022-09-22 15:52:43 +0100

    jquery

---
+++ index.js
@@ -0,0 +1,38 @@
+// $("h1").click(function(){
+//     $("h1").html("HELLO GUYS HOPE YOU DOING WELL")
+// })
+
+// for (var i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click",
+//     function (){
+//         document.querySelector("h1").style.color ="red"
+//     });
+    
+// }
+    // $("button").click(function () {
+    //     $("h1").css("color", "black")
+    // });
+
+    // adding keyPress using jquery
+
+    // $(document).keydown(function (event) {
+    //     $("h1").text(event.key)
+    // })
+
+    // other methods fo manipulating in jQuery
+
+    $("h1").on("click", function () {
+        $("h1").css("color", "black")
+    })
+
+    // adding prepend, append, before and after
+
+    $("h1").before("<button>new</button>");
+    $("h1").after("<button>new</button>");
+    $("h1").append("<button>new</button>");
+    $("h1").prepend("<button>new</button>");
+
+    // to remove
+
+    // $("button").remove();
+
